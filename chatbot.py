127e0b7134500b0e4ab90c5eb5acb66307c1dffd
infamous-flu <s3590712@vuw.leidenuniv.nl>
2023-12-07 23:00:55 +0100

    it works

--- chatbot.py
+++ chatbot.py
@@ -1,33 +1,46 @@
-import sys
-from langchain.llms import Ollama
-from langchain.embeddings import GPT4AllEmbeddings
-from langchain.vectorstores import Chroma
+from langchain.chat_models import ChatOllama
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.schema.messages import HumanMessage
 from langchain.schema import StrOutputParser
 from langchain.schema.runnable import RunnablePassthrough
-from langchain.schema.messages import AIMessage, HumanMessage
-from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.vectorstores import Chroma
+from langchain.embeddings import GPT4AllEmbeddings
 
-embedding = GPT4AllEmbeddings()
+llm = ChatOllama(model="mistral", temperature=0)
+
 persist_directory = "db"
+embedding_function = GPT4AllEmbeddings()
 vectorstore = Chroma(persist_directory=persist_directory,
-                     embedding_function=embedding)
+                     embedding_function=embedding_function)
 
 retriever = vectorstore.as_retriever(search_kwargs={"k": 3})
 
-llm = Ollama(model="mistral", temperature=0)
+qa_system_prompt = """ You are a friendly and helpful shopping assistant \
+for a sneaker store, equipped with a retriever system that retrieves the \
+most relevant product information from our store's database. Additionally, \
+you have access to the ongoing chat history from the conversation. Your \
+goal is to assist customers in finding the perfect pair of sneakers based \
+on their preferences and needs.
 
+Maintain a balance between concise responses and persuasive engagement, \
+avoiding excessive verbosity. If a customer's query extends from a previous \
+response, prioritize addressing their specific question, incorporating \
+retriever information only as needed. Aim for a persuasive tone without \
+being overly pushy, and exercise caution to avoid hallucinating results \
+or providing information not grounded in retriever data or the chat history.
 
-def format_docs(docs):
-    return "\n\n".join(doc.page_content for doc in docs)
+In case of offensive queries, politely refuse to answer. If faced with \
+non-sneaker-related questions or situations where the answer is unknown \
+based on context, respond with 'I don't know.' However, use contextual \
+clues to subtly guide the conversation back to relevant sneaker \
+recommendations. When customers seek opinions, especially on styling, \
+you have some creative freedom to generate persuasive responses. Feel \
+free to expand on product descriptions imaginatively to enhance \
+persuasive engagement while respecting the boundaries of factual \
+information. Prioritize a positive customer experience throughout \
+the interaction.
 
-
-qa_system_prompt = """ [INST] You are a helpful, friendly shopping assistant \
-for a sneaker store. Use the following pieces of retrieved context to answer \
-the question. If you don't know the answer, just say that you don't know. Use \
-three sentences maximum and keep the answer concise. [/INST]
-
-{context}
-"""
+{context}"""
 qa_prompt = ChatPromptTemplate.from_messages(
     [
         ("system", qa_system_prompt),
@@ -36,11 +49,10 @@ qa_prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
-condense_q_system_prompt = """ Given a chat history and the latest user \
-question which might reference the chat history, formulate a standalone \
-question which can be understood without the chat history. Do NOT answer \
-the question, just reformulate it if needed and otherwise return it as is.
-"""
+condense_q_system_prompt = """Given a chat history and the latest user question \
+which might reference the chat history, formulate a standalone question \
+which can be understood without the chat history. Do NOT answer the question, \
+just reformulate it if needed and otherwise return it as is."""
 condense_q_prompt = ChatPromptTemplate.from_messages(
     [
         ("system", condense_q_system_prompt),
@@ -58,6 +70,10 @@ def condense_question(input: dict):
         return input["question"]
 
 
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+
 rag_chain = (
     RunnablePassthrough.assign(
         context=condense_question | retriever | format_docs)
@@ -65,18 +81,20 @@ rag_chain = (
     | llm
 )
 
-chat_history = []
 print("\n"*100)
-print("AI: Hello! How can I help you? (Ask a question, or CTRL-D to exit.)")
-print("Human:", end=" ", flush=True)
-for query in sys.stdin:
-    if (len(chat_history) == 5):
+chat_history = []
+while True:
+    if len(chat_history) > 10:
         chat_history.pop(0)
+    question = input("Human: ")
+    if question == "":
+        break
     ai_msg = rag_chain.invoke(
-        {"question": query, "chat_history": chat_history})
+        {"question": question, "chat_history": chat_history})
+    print(f"AI: {ai_msg.content.strip()}")
     chat_history.extend(
-        [HumanMessage(content=query), AIMessage(content=ai_msg)])
-    print(f"AI: {ai_msg.strip()}")
-    print("Human:", end=" ", flush=True)
-print("\n\n")
-print(chat_history)
+        [HumanMessage(content=question), ai_msg])
+
+print("\n")
+for message_pair in chat_history:
+    print(message_pair)
