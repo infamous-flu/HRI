ab753239969aeb891baf4af0a0c347d7dd3ee616
infamous-flu <s3590712@vuw.leidenuniv.nl>
2023-12-15 23:51:31 +0100

    added comments so you know what's what

--- chatbot.py
+++ chatbot.py
@@ -1,3 +1,7 @@
+################################################################################################
+
+"""REQUIRED MODULES"""
+
 from langchain.vectorstores import Chroma
 from langchain.embeddings import GPT4AllEmbeddings
 from langchain.llms import Ollama
@@ -13,10 +17,18 @@ from langchain.schema.runnable import RunnablePassthrough
 from langchain.memory import ConversationSummaryBufferMemory
 from pprint import pprint
 
+################################################################################################
+
+"""LLM AND CHAT MODEL CONFIGURATION"""
+
 model = "mistral"
 
 chat = ChatOllama(model=model, temperature=0)
 llm = Ollama(model=model, temperature=0)
+
+################################################################################################
+
+"""VECTORSTORE RETRIEVER CONFIGURATION"""
 
 persist_directory = "db"
 embedding_function = GPT4AllEmbeddings()
@@ -24,6 +36,12 @@ vectorstore = Chroma(persist_directory=persist_directory,
                      embedding_function=embedding_function)
 
 retriever = vectorstore.as_retriever(search_kwargs={"k": 3})
+
+################################################################################################
+
+"""QA PROMPT CONFIGURATION"""
+
+# TODO: CHANGE THE SYSTEM PROMPT HERE TO SEE IF WE GET BETTER RESPONSESs
 
 qa_system_prompt = """As an expert AI shopping assistant specializing in sneakers, leverage \
 your persuasive skills and our powerful retriever system to enhance customer engagement and \
@@ -44,6 +62,10 @@ qa_prompt = ChatPromptTemplate(
     ]
 )
 
+################################################################################################
+
+"""CONDENSE QUESTION PROMPT CONFIGURATION"""
+
 condense_q_system_prompt = """Given a chat history and the latest user question \
 which may reference the chat history, formulate a standalone question which can \
 be understood without the chat history. Do NOT answer the question, just reformulate \
@@ -55,7 +77,16 @@ condense_q_prompt = ChatPromptTemplate(
         HumanMessagePromptTemplate.from_template("{question}")
     ]
 )
+
+################################################################################################
+
+"""CONDENSE QUESTION CHAIN CONFIGURATION"""
+
 condense_q_chain = condense_q_prompt | chat | StrOutputParser()
+
+################################################################################################
+
+"""HELPER FUNCTIONS"""
 
 
 def condense_question(input: dict):
@@ -69,6 +100,10 @@ def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
 
 
+################################################################################################
+
+"""RAG CHAIN CONFIGURATION"""
+
 rag_chain = (
     RunnablePassthrough.assign(
         context=condense_question | retriever | format_docs)
@@ -76,6 +111,15 @@ rag_chain = (
     | chat
     | StrOutputParser()
 )
+
+################################################################################################
+
+"""WELCOME MESSAGE AND FIRST TIME MESSAGE"""
+
+# TODO: WRITE A BETTER WELCOME MESSAGE AND FIRST TIME MESSAGE!
+
+# WELCOME MESSAGE : INFORMATION ABOUT THE ROBOT AND STORE, CREATE ILLUSION OF FRIENDLINESS
+# FIRST TIME MESSAGE: INSTRUCTIONS TO HUMAN ON HOW TO INTERACT WITH THE ROBOT AND WHAT TO ASK
 
 welcome_msg = """I am your NAO shopping assistant. Feel free to ask me \
 any questions you have about our fabulous collection. I can help you find the \
@@ -88,30 +132,55 @@ My eyes will light up green when I'm listening and red when I've stopped. \
 Say "goodbye" if you want to stop the interaction. Do you have a question?
 """
 
+################################################################################################
+
+"""MEMORY CONFIGURATION"""
+
 memory = ConversationSummaryBufferMemory(
     llm=llm, max_token_limit=400, return_messages=True
 )
 memory.save_context({"input": ""}, {"output": welcome_msg+first_time_msg})
 
-print("\n"*100)
-print("AI: " + welcome_msg + first_time_msg)
-while True:
-    chat_history = memory.load_memory_variables({}).get("history", [])
-    question = input("Human: ")
-    if question == "":
-        break
-    if question == "/memory":
-        pprint(f"{chat_history}")
-        print()
-        continue
-    question += " Keep your response under 80 words."
-    ai_msg = rag_chain.invoke(
-        {"question": question, "chat_history": chat_history})
-    print(f"AI: {ai_msg}\n")
-    memory.save_context({"input": question}, {"output": ai_msg})
+################################################################################################
 
-goodbye_msg = """
-AI: Goodbye, we hope you find the perfect sneakers for your needs at our store. \
-If you have any questions or need assistance, feel free to reach out again.
-"""
-print(goodbye_msg)
+"""MAIN FUNCTION"""
+
+
+def main():
+    print("\n"*100)
+    print("AI: " + welcome_msg + first_time_msg)
+    while True:
+        chat_history = memory.load_memory_variables({}).get("history", [])
+        question = input("Human: ")
+        if question == "":
+            break
+        if question == "/memory":
+            pprint(f"{chat_history}")
+            print()
+            continue
+
+################################################################################################
+
+        # TODO: ADDITIONAL INSTRUCTION APPENDED AFTER EACH HUMAN MESSAGE
+
+        #   BE CAREFUL!!! THIS IS MUCH STRONGER THAN CHANGING THE SYSTEM
+        #   PROMPT AND CAN REALLY LEAD TO DUMB AI RESPONSES
+
+        question += " Keep your response under 80 words."
+
+###############################################################################################
+
+        ai_msg = rag_chain.invoke(
+            {"question": question, "chat_history": chat_history})
+        print(f"AI: {ai_msg}\n")
+        memory.save_context({"input": question}, {"output": ai_msg})
+
+    goodbye_msg = """
+    AI: Goodbye, we hope you find the perfect sneakers for your needs at our store. \
+    If you have any questions or need assistance, feel free to reach out again.
+    """
+    print(goodbye_msg)
+
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import json
+################################################################################################
+
+"""REQUIRED MODULES"""
+
 from autobahn.twisted.component import Component, run
 from autobahn.wamp.exception import ApplicationError
 from twisted.internet.defer import inlineCallbacks
@@ -17,13 +20,20 @@ from langchain.prompts import (
 from langchain.schema import StrOutputParser
 from langchain.schema.runnable import RunnablePassthrough
 from langchain.memory import ConversationSummaryBufferMemory
-from random import choice
 from pprint import pprint
+
+################################################################################################
+
+"""LLM AND CHAT MODEL CONFIGURATION"""
 
 model = "mistral"
 
-chat = ChatOllama(model=model, temperature=0.1)
+chat = ChatOllama(model=model, temperature=0)
 llm = Ollama(model=model, temperature=0)
+
+################################################################################################
+
+"""VECTORSTORE RETRIEVER CONFIGURATION"""
 
 persist_directory = "db"
 embedding_function = GPT4AllEmbeddings()
@@ -31,6 +41,12 @@ vectorstore = Chroma(persist_directory=persist_directory,
                      embedding_function=embedding_function)
 
 retriever = vectorstore.as_retriever(search_kwargs={"k": 3})
+
+################################################################################################
+
+"""QA PROMPT CONFIGURATION"""
+
+# TODO: CHANGE THE SYSTEM PROMPT HERE TO SEE IF WE GET BETTER RESPONSESs
 
 qa_system_prompt = """As an expert AI shopping assistant specializing in sneakers, leverage \
 your persuasive skills and our powerful retriever system to enhance customer engagement and \
@@ -51,6 +67,10 @@ qa_prompt = ChatPromptTemplate(
     ]
 )
 
+################################################################################################
+
+"""CONDENSE QUESTION PROMPT CONFIGURATION"""
+
 condense_q_system_prompt = """Given a chat history and the latest user question \
 which may reference the chat history, formulate a standalone question which can \
 be understood without the chat history. Do NOT answer the question, just reformulate \
@@ -62,7 +82,16 @@ condense_q_prompt = ChatPromptTemplate(
         HumanMessagePromptTemplate.from_template("{question}")
     ]
 )
+
+################################################################################################
+
+"""CONDENSE QUESTION CHAIN CONFIGURATION"""
+
 condense_q_chain = condense_q_prompt | chat | StrOutputParser()
+
+################################################################################################
+
+"""HELPER FUNCTIONS"""
 
 
 def condense_question(input: dict):
@@ -76,6 +105,10 @@ def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
 
 
+################################################################################################
+
+"""RAG CHAIN CONFIGURATION"""
+
 rag_chain = (
     RunnablePassthrough.assign(
         context=condense_question | retriever | format_docs)
@@ -84,30 +117,44 @@ rag_chain = (
     | StrOutputParser()
 )
 
-#################################################################################
+################################################################################################
+
+"""WELCOME MESSAGE, FIRST TIME, AND GOODBYE MESSAGE"""
 
 # TODO: WRITE A BETTER WELCOME MESSAGE AND FIRST TIME MESSAGE!
 
-#################################################################################
+# WELCOME MESSAGE : INFORMATION ABOUT THE ROBOT AND STORE, CREATE ILLUSION OF FRIENDLINESS
+# FIRST TIME MESSAGE: INSTRUCTIONS TO HUMAN ON HOW TO INTERACT WITH THE ROBOT AND WHAT TO ASK
 
 welcome_msg = """I am your NAO shopping assistant. Feel free to ask me \
 any questions you have about our fabulous collection. I can help you find the \
-perfect pair of sneakers that match your style and preferences.
-"""
+perfect pair of sneakers that match your style and preferences."""
 
 first_time_msg = f""" You have 10 seconds for each of your questions, please \
 keep your questions short and concise. Wait half a second after each of my \
 responses before asking your question to ensure optimal speech recognition. \
 My eyes will light up green when I'm listening and red when I've stopped. \
-Say "goodbye" if you want to stop the interaction. Do you have a question?"""
+Say "goodbye" if you want to stop the interaction. Do you have a question?
+"""
 
-#################################################################################
+goodbye_msg = """Goodbye! I hope you find the perfect sneakers for your needs \
+at our store. If you have any more questions or need further assistance, feel \
+free to reach out again.
+"""
+
+################################################################################################
+
+"""MEMORY CONFIGURATION"""
 
 memory = ConversationSummaryBufferMemory(
     llm=llm, max_token_limit=400, return_messages=True
 )
 memory.save_context(
     {"input": ""}, {"output": welcome_msg+first_time_msg})
+
+################################################################################################
+
+"""SPEECH RECOGNITION FUNCTION"""
 
 
 def transcribe_file(speech_file: str):
@@ -130,9 +177,17 @@ def transcribe_file(speech_file: str):
     except Exception as e:
         return ""
 
+################################################################################################
+
+
+"""MAIN FUNCTION"""
+
 
 @inlineCallbacks
 def main(session, details):
+
+    ############ INITIALIZATION ############
+
     print("Starting up...")
     print("Waiting for cloud modules")
     ready = False
@@ -144,59 +199,110 @@ def main(session, details):
             print("Cloud modules are not ready yet")
             yield sleep(0.25)
     print("Cloud modules are ready to go!")
+
+    ############# INTRODUCTION #############
+
+    # Play the BlocklyStand behavior to set the initial pose
     yield session.call("rom.optional.behavior.play", name="BlocklyStand")
+    # Set the eyes' color to blue
     yield session.call("rom.actuator.light.write", mode="linear", frames=[
         {"time": 0000, "data": {"body.head.eyes": [0, 0, 255]}}],)
+    # Configure the dialogue system to use the English (UK) language
     yield session.call("rie.dialogue.config.language", "en_uk")
+    # Set the audio volume to maximum (100)
     yield session.call("rom.actuator.audio.volume", 100)
+    # Play a behavior (BlocklyWaveRightArm) to wave at the customer
     session.call("rom.optional.behavior.play", name="BlocklyWaveRightArm")
+    # Say a welcome message to the customer
     yield session.call("rie.dialogue.say", text="Welcome to our sneaker store!")
     yield session.call("rie.dialogue.say_animated", text=welcome_msg+first_time_msg)
-    answer = ""
+
+    ############## MAIN LOOP ###############
+
     while True:
+        # Set the eyes' color to green while listening
         yield session.call("rom.actuator.light.write", mode="linear", frames=[
             {"time": 0000, "data": {"body.head.eyes": [0, 255, 0]}}],)
+        # Print a message indicating that the system is listening
         print("Listening...")
+        # Read audio frames from the sensor for a specified time duration (8 seconds)
         frames = yield session.call("rom.sensor.hearing.read", time=8000)
+        # Combine audio data from all frames into a single byte string
         audio_data = b""
         for frame in frames:
             audio_data += frame["data"].get("body.head.front", b"")
+        # Write the combined audio data to a raw audio file ("output.raw")
         with open("output.raw", "wb") as raw_file:
             raw_file.write(audio_data)
+        # Print a message indicating that the system has stopped listening
         print("Stopped listening")
+        # Set the eyes' color to red after listening
         yield session.call("rom.actuator.light.write", mode="linear", frames=[
             {"time": 0000, "data": {"body.head.eyes": [255, 0, 0]}}],)
+        # Transcribe the recorded audio file to obtain the user's question
         question = transcribe_file(f"output.raw")
+        # Check if the user wants to end the conversation
         if "goodbye" in question or "bye" in question:
             break
+        # If no question is detected, prompt the user to speak louder and more clearly
         if question == "":
-            yield session.call("rom.actuator.audio.stop")
+            # Prompt the user to speak louder and more clearly
             answer = "Sorry, I didn't quite catch what you were saying. Can you speak louder and more clearly?"
+            # Play a behavior (BlocklyShrug) to express confusion or uncertainty
             session.call("rom.optional.behavior.play", name="BlocklyShrug")
+            # Set the eyes' color to blue
             yield session.call("rom.actuator.light.write", mode="linear", frames=[
                 {"time": 0000, "data": {"body.head.eyes": [0, 0, 255]}}],)
+            # Speak the apology to the user
             yield session.call("rie.dialogue.say", text=answer)
+            # Continue to the next iteration of the loop
             continue
+
+        ########################################################################################
+
+        """ADDITIONAL INSTRUCTION APPENDED AFTER EACH HUMAN MESSAGE"""
+
+        # TODO: MAYBE CHANGE THIS TO GET A BETTER RESPONSE FROM THE AI?
+
+        #   BE CAREFUL!!! THIS IS MUCH STRONGER THAN CHANGING THE SYSTEM
+        #   PROMPT AND CAN REALLY LEAD TO DUMB AI RESPONSES
+
         question += " Keep your response under 80 words."
+
+        ########################################################################################
+
+        # Load chat history from memory variables
         chat_history = memory.load_memory_variables({}).get("history", [])
+        # Use the chat model (rag_chain) to generate a response based on the user's question
         answer = rag_chain.invoke(
             {"question": question, "chat_history": chat_history})
+        # Print the generated answer to the console
         print(answer)
+        # Save the user's question and the generated answer to the context memory
         memory.save_context({"input": question}, {"output": answer})
+        # Set the eyes' color to blue
         yield session.call("rom.actuator.light.write", mode="linear", frames=[
             {"time": 0000, "data": {"body.head.eyes": [0, 0, 255]}}],)
-        session.call("rom.optional.behavior.play", name="BlocklyWaveRightArm")
+        # Speak the generated answer in an animated manner
         yield session.call("rie.dialogue.say_animated", text=answer)
+
+    ############### GOODBYE ################
+
+    # Set the eyes' color to blue
     yield session.call("rom.actuator.light.write", mode="linear", frames=[{"time": 0000, "data": {"body.head.eyes": [0, 0, 255]}}],)
-    goodbye_msg = """Goodbye! I hope you find the perfect sneakers for your needs \
-    at our store. If you have any more questions or need further assistance, feel \
-    free to reach out again.
-    """
+    # Speak the goodbye message in an animated manner
     yield session.call("rie.dialogue.say_animated", text=goodbye_msg)
+    # Play the BlocklyBow behavior to perform a bowing motion
     session.call("rom.optional.behavior.play", name="BlocklyBow")
+    # End the session
     session.leave()
+    # Print the chat history to the console
     pprint(chat_history)
 
+
+################################################################################################
+
+"""WAMP CONFIGURATION"""
 
 wamp = Component(
     transports=[{
@@ -208,6 +314,9 @@ wamp = Component(
 )
 
 wamp.on_join(main)
+
+################################################################################################
+
 
 if __name__ == "__main__":
     run([wamp])
